fa41c2fc73a508c045980c46f7b7a9c37e023bae
Lukas Möller <lukasmoeller1998@googlemail.com>
2020-03-17 19:30:43 +0100

    Added Custom Sheet Class for PDFExists Method

---
+++ CoreLibrary/Entities/Custom/Sheet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Zebra.Library
+{
+    partial class Sheet
+    {
+        /// <summary>
+        /// Checks, if the PDF File exists using the passed DBManager
+        /// </summary>
+        public bool FileExists(ZebraDBManager manager)
+        {
+            try
+            {
+                manager.Archive.GetFile(this);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+    }
+}
