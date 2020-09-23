f0a975aa1e7875f6acc71e8de2ad6c43d5de2822
Lukas Möller <lukasmoeller1998@googlemail.com>
2020-09-23 15:10:15 +0200

    Changed the File Structure in the Local archive. Sheets are now Placed into the folder with SheedID as Name

--- CoreLibrary/Settings/LocalArchive.cs
+++ CoreLibrary/Settings/LocalArchive.cs
@@ -27,14 +27,20 @@ namespace Zebra.Library
 
         public override void PushFile(FileInfo file, Sheet sheet, FileImportMode mode = FileImportMode.Copy)
         {
+            //Check if destination file already exists
+            if (File.Exists(Path.FullName + "\\" + sheet.SheetID.ToString("00000000") + ".pdf"))
+            {
+                throw new System.IO.IOException("File already exists");
+            }
+            
             switch (mode)
             {
                 case FileImportMode.Copy:
-                    if (!Directory.Exists(Path.FullName + "\\" + sheet.Piece.PieceID)) Directory.CreateDirectory(Path.FullName + "\\" + sheet.Piece.PieceID);
-                    file.CopyTo(Path.FullName + "\\" + sheet.Piece.PieceID + "\\" + sheet.SheetID + ".pdf", false);
+                    //if (!Directory.Exists(Path.FullName + "\\" + sheet.Piece.PieceID)) Directory.CreateDirectory(Path.FullName + "\\" + sheet.Piece.PieceID);
+                    file.CopyTo(Path.FullName + "\\" + sheet.SheetID.ToString("00000000") + ".pdf", false);
                     break;
                 case FileImportMode.Move:
-                    file.MoveTo(Path.FullName + "\\" + sheet.Piece.PieceID + "\\" + sheet.SheetID + ".pdf", false);
+                    file.MoveTo(Path.FullName + "\\" + sheet.SheetID.ToString("00000000") + ".pdf", false);
                     break;
                 default:
                     break;
