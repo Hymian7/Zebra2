f6011b74d62ab812c6b6f48a0c3e19770d773937
Lukas Möller <lukasmoeller1998@googlemail.com>
2020-09-30 13:31:15 +0200

    Ported frmNewConfig to MVVM

---
+++ ZebraDesktop/Enums/EnumSelection.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ZebraDesktop.Enums
+{
+    public class EnumSelection<T> : INotifyPropertyChanged where T : struct, IComparable, IFormattable, IConvertible
+    {
+        private T value; // stored value of the Enum
+        private bool isFlagged; // Enum uses flags?
+        private bool canDeselect; // Can be deselected? (Radio buttons cannot deselect, checkboxes can)
+        private T blankValue; // what is considered the "blank" value if it can be deselected?
+
+        public EnumSelection(T value) : this(value, false, default(T)) { }
+        public EnumSelection(T value, bool canDeselect) : this(value, canDeselect, default(T)) { }
+        public EnumSelection(T value, T blankValue) : this(value, true, blankValue) { }
+        public EnumSelection(T value, bool canDeselect, T blankValue)
+        {
+            if (!typeof(T).IsEnum) throw new ArgumentException($"{nameof(T)} must be an enum type"); // I really wish there was a way to constrain generic types to enums...
+            isFlagged = typeof(T).IsDefined(typeof(FlagsAttribute), false);
+
+            this.value = value;
+            this.canDeselect = canDeselect;
+            this.blankValue = blankValue;
+        }
+
+        public T Value
+        {
+            get { return value; }
+            set
+            {
+                if (this.value.Equals(value)) return;
+                this.value = value;
+                NotifyPropertyChanged();
+                NotifyPropertyChanged("Item[]"); // Notify that the indexer property has changed
+            }
+        }
+
+        [IndexerName("Item")]
+        public bool this[T key]
+        {
+            get
+            {
+                int iKey = (int)(object)key;
+                return isFlagged ? ((int)(object)value & iKey) == iKey : value.Equals(key);
+            }
+            set
+            {
+                if (isFlagged)
+                {
+                    int iValue = (int)(object)this.value;
+                    int iKey = (int)(object)key;
+
+                    if (((iValue & iKey) == iKey) == value) return;
+
+                    if (value)
+                        Value = (T)(object)(iValue | iKey);
+                    else
+                        Value = (T)(object)(iValue & ~iKey);
+                }
+                else
+                {
+                    if (this.value.Equals(key) == value) return;
+                    if (!value && !canDeselect) return;
+
+                    Value = value ? key : blankValue;
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- ZebraDesktop/Views/frmNewConfig.xaml.cs
+++ ZebraDesktop/Views/frmNewConfig.xaml.cs
@@ -24,39 +24,5 @@ namespace ZebraDesktop
             InitializeComponent();
         }
 
-        private void btnBrowseDBPath_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.ShowDialog();
-            if (!(ofd.FileName == null))
-            {
-                tbDBPath.Text = ofd.FileName;
-            }
-
-            
-        }
-
-        private void btnBrowseArchivePath_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Einfach Pfad in die Textbox kopieren");
-        }
-        private void btnBrowseTempDirPath_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Einfach Pfad in die Textbox kopieren");
-        }
-
-        private void btnOK_Click(object sender, RoutedEventArgs e)
-        {
-            if (!(tbConfigName.Text =="" || tbDBPath.Text == "" || tbArchivePath.Text == ""))
-            {
-                Zebra.Library.ZebraConfig newconf = new Zebra.Library.ZebraConfig(tbConfigName.Text, Zebra.Library.DatabaseProvider.SQLite, new Zebra.Library.SQLiteCredentials(tbDBPath.Text), Zebra.Library.ArchiveType.Local, new Zebra.Library.LocalArchiveCredentials(tbArchivePath.Text), tbTempDirPath.Text);
-
-                if (newconf.Serialize(@"F:\GitHub\Zebra2\ZebraDesktop\bin\Debug\netcoreapp3.1\configs"))
-                {
-                    this.Close();
-                }
-            }
-        }
-
     }
 }
