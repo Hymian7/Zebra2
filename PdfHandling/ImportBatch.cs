099b449d691ced60cceec071ddca29ba4f5e2e3c
Lukas Möller <lukasmoeller1998@googlemail.com>
2020-10-13 12:54:48 +0200

    Added possibility to Load the import Candidates in an Import Batch asynchronously

--- PdfHandling/ImportBatch.cs
+++ PdfHandling/ImportBatch.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
 
 namespace Zebra.PdfHandling
 {
@@ -15,22 +16,19 @@ namespace Zebra.PdfHandling
     {
         public FileInfo File { get; set; }
         public PreviewablePdfDocument Document { get; set; }
-        public List<ImportAssignment> importAssignments { get; set; }
+        public ObservableCollection<ImportAssignment> importAssignments { get; set; }
 
-        public List<ImportCandidate> importCandidates { get; set; }
+        public ObservableCollection<ImportCandidate> importCandidates { get; set; }
 
         public ImportBatch(string filename)
         {
             File = new FileInfo(filename);
             Document = new PreviewablePdfDocument(File.FullName);
 
-            importAssignments = new List<ImportAssignment>();
-            importCandidates = new List<ImportCandidate>();
+            importAssignments = new ObservableCollection<ImportAssignment>();
+            importCandidates = new ObservableCollection<ImportCandidate>();
 
-            for (int i = 0; i < Document.PageCount; i++)
-            {
-                importCandidates.Add(new ImportCandidate(i, Document.Pages[i].Thumbnail));
-            }
+            //LoadImportCandidates();
 
         }
 
@@ -39,6 +37,39 @@ namespace Zebra.PdfHandling
         protected void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Sends generated ImportCandidates back to UI Thread in the ImportBatchProgressReport
+        /// Items have to be added to Collection manually in UI Thread
+        /// </summary>
+        /// <param name="progress"></param>
+        /// <returns></returns>
+        public async Task LoadImportCandidatesAsync(IProgress<ImportBatchProgressReport> progress)
+        {
+            ImportBatchProgressReport report = new ImportBatchProgressReport();
+
+            for (int i = 0; i < Document.PageCount; i++)
+            {
+                await Task.Run(() => report.LastImported = (new ImportCandidate(i, Document.Pages[i].Thumbnail)));
+                report.ImportedCandidates.Add(report.LastImported);
+                report.Percentage=((int)(i + 1) * 100 / Document.PageCount);
+                report.GenerateMessage(i + 1, Document.PageCount);
+
+                progress.Report(report);
+
+            }
+            report.Percentage = 0;
+            report.Message = "Import erfolgreich!";
+            progress.Report(report);
+        }
+
+        public void LoadImportCandidates()
+        {
+            for (int i = 0; i < Document.PageCount; i++)
+            {
+                importCandidates.Add(new ImportCandidate(i, Document.Pages[i].Thumbnail));
+            }
         }
 
         public async Task ImportAllAssignments(ZebraDBManager manager)
