be218d6303f03c55465a490ca20ae44882618791
Lukas Möller <lukasmoeller1998@googlemail.com>
2020-10-13 10:39:42 +0200

    Fixed disabled Button in BatchImporter Form

---
+++ ZebraDesktop/ViewModels/PDFBatchImporterViewModel.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using Zebra.Library;
+using Zebra.PdfHandling;
+
+namespace ZebraDesktop.ViewModels
+{
+    public class PDFBatchImporterViewModel : ViewModelBase
+    {
+        #region Properties
+
+        private ZebraDBManager Manager { get { return (Application.Current as App).Manager; } }
+
+        private ObservableCollection<Piece> _allPieces;
+        public ObservableCollection<Piece> AllPieces
+        {
+            get { return _allPieces; }
+            set { _allPieces = value; NotifyPropertyChanged(); }
+        }
+        
+        private ObservableCollection<Part> _allParts;
+        public ObservableCollection<Part> AllParts
+        {
+            get { return _allParts; }
+            set { _allParts = value; NotifyPropertyChanged(); }
+        }
+
+        private ImportBatch _batch;
+
+        public ImportBatch Batch
+        {
+            get { return _batch; }
+            set { _batch = value; NotifyPropertyChanged();}
+        }
+
+        private ImportCandidate _selectedItem;
+
+        public ImportCandidate SelectedItem
+        {
+            get { return _selectedItem; }
+            set { _selectedItem = value; NotifyPropertyChanged(); UpdateButtonStatus(); }
+        }
+
+        private DelegateCommand _openFileCommand;
+
+        public DelegateCommand OpenFileCommand
+        {
+            get { return _openFileCommand; }
+            set { _openFileCommand = value; NotifyPropertyChanged(); }
+        }
+
+        private DelegateCommand _importCommand;
+
+        public DelegateCommand ImportCommand
+        {
+            get { return _importCommand; }
+            set { _importCommand = value; NotifyPropertyChanged(); }
+        }
+
+        private DelegateCommand _assignCommand;
+
+        public DelegateCommand AssignCommand
+        {
+            get { return _assignCommand; }
+            set { _assignCommand = value; NotifyPropertyChanged(); }
+        }
+
+
+
+
+        #endregion
+
+        #region Constructors
+
+        public PDFBatchImporterViewModel()
+        {
+            Manager.Context.Piece.Load();
+            AllPieces = Manager.Context.Piece.Local.ToObservableCollection();
+
+            Manager.Context.Part.Load();
+            AllParts = Manager.Context.Part.Local.ToObservableCollection();
+
+            OpenFileCommand = new DelegateCommand(executeOpenFileCommand, canExecuteOpenFileCommand);
+            ImportCommand = new DelegateCommand(executeImportCommand, canExecuteImportCommand);
+            AssignCommand = new DelegateCommand(executeAssignCommand, canExecuteAssignCommand);
+
+        }
+
+
+        #endregion
+
+        #region Commands
+        private void executeOpenFileCommand(object obj)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Filter = "PDF Files | *.pdf";
+            ofd.Multiselect = false;
+
+
+            ofd.ShowDialog();
+
+            if (System.IO.File.Exists(ofd.FileName))
+            {
+
+                Batch = new ImportBatch(ofd.FileName);
+                Batch.PropertyChanged += base.UpdateButtonStatus;
+
+            }
+        }
+
+        private bool canExecuteOpenFileCommand(object obj)
+        {
+            return true;
+        }
+
+        private void executeImportCommand(object obj)
+        {
+            var frm = new frmPdfBatchImportPreview(Batch);
+            frm.ShowDialog();
+        }
+
+        private bool canExecuteImportCommand(object obj)
+        {
+            return Batch != null ? (Batch.importAssignments.Count > 0) : false ;
+        }
+
+        private void executeAssignCommand(object obj)
+        {
+ 
+        }
+
+        private bool canExecuteAssignCommand(object obj)
+        {
+            //return (SelectedItem?.AssignedPart != null && SelectedItem?.AssignedPiece != null);
+            return true;
+        }
+
+        #endregion
+
+
+    }
+}
--- ZebraDesktop/Views/frmPdfBatchImporter.xaml.cs
+++ ZebraDesktop/Views/frmPdfBatchImporter.xaml.cs
@@ -35,79 +35,43 @@ namespace ZebraDesktop
         {
             InitializeComponent();
 
-            context.Piece.Load();
-            AllPieces = context.Piece.Local.ToObservableCollection();
-
-            context.Part.Load();
-            AllParts = context.Part.Local.ToObservableCollection();
-
-            cbAssPart.ItemsSource = AllParts;
-            cbAssPiece.ItemsSource = AllPieces;
-
-        }
-
-
-
-        private async void btnOpen_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-
-            ofd.Filter = "PDF Files | *.pdf";
-            ofd.Multiselect = false;
-
-
-            ofd.ShowDialog();
-
-            if (System.IO.File.Exists(ofd.FileName))
-            {
-                lbThumbnails.ItemsSource = null;
-
-                this.Cursor = Cursors.Wait;
-                await Task.Run(()=> batch = new ImportBatch(ofd.FileName));
-
-                lbThumbnails.ItemsSource = batch.importCandidates;
-
-                this.Cursor = Cursors.Arrow;
-
-
-            }
         }
 
         private void lbThumbnails_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
-            if (lbThumbnails.SelectedIndex != -1)
-            {
-                imgPreview.Source = batch.Document.Pages[(lbThumbnails.SelectedItem as ImportCandidate).PageNumber].RenderedPage.ConvertToImageSource();
+            //if (lbThumbnails.SelectedIndex != -1)
+            //{
+            //    imgPreview.Source = batch.Document.Pages[(lbThumbnails.SelectedItem as ImportCandidate).PageNumber].RenderedPage.ConvertToImageSource();
 
-                if ((lbThumbnails.SelectedItem as ImportCandidate).AssignedPart != null)
-                {
-                    cbAssPart.SelectedItem = (lbThumbnails.SelectedItem as ImportCandidate).AssignedPart;
-                    //cbAssPart.Text = (lbThumbnails.SelectedItem as ImportCandidate).AssignedPart.Name;
-                    cbAssPiece.SelectedItem = (lbThumbnails.SelectedItem as ImportCandidate).AssignedPiece;
-                }
-                else
-                {
-                    if(lbThumbnails.SelectedIndex >0) cbAssPiece.SelectedItem = (lbThumbnails.Items[lbThumbnails.SelectedIndex-1] as ImportCandidate).AssignedPart;
-                    cbAssPart.SelectedItem = null;
-                }
+            //    if ((lbThumbnails.SelectedItem as ImportCandidate).AssignedPart != null)
+            //    {
+            //        cbAssPart.SelectedItem = (lbThumbnails.SelectedItem as ImportCandidate).AssignedPart;
+            //        //cbAssPart.Text = (lbThumbnails.SelectedItem as ImportCandidate).AssignedPart.Name;
+            //        cbAssPiece.SelectedItem = (lbThumbnails.SelectedItem as ImportCandidate).AssignedPiece;
+            //    }
+            //    else
+            //    {
+            //        if(lbThumbnails.SelectedIndex >0) cbAssPiece.SelectedItem = (lbThumbnails.Items[lbThumbnails.SelectedIndex-1] as ImportCandidate).AssignedPart;
+            //        cbAssPart.SelectedItem = null;
+            //    }
 
-            }
+            //}
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            (lbThumbnails.SelectedItem as ImportCandidate).AssignedPart = (cbAssPart.SelectedItem as Part);
-            (lbThumbnails.SelectedItem as ImportCandidate).AssignedPiece = (cbAssPiece.SelectedItem as Piece);
+            // Todo: Make this optional/selectable in Preferences
+            if (lbThumbnails.SelectedIndex < lbThumbnails.Items.Count - 1)
+            {
+                lbThumbnails.SelectedItem = lbThumbnails.Items[lbThumbnails.SelectedIndex+1];
+            }
+            else
+            {
+                lbThumbnails.SelectedItem = lbThumbnails.Items[0];
+            }
 
-
-            lbThumbnails.SelectedIndex++;
             cbAssPiece.Focus();
         }
 
-        private void btnImport_Click(object sender, RoutedEventArgs e)
-        {
-            var frm = new frmPdfBatchImportPreview(batch);
-            frm.ShowDialog();
-        }
     }
 }
