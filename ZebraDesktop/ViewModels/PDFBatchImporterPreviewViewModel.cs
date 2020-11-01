8d98d922fb7ae3115e312e526150eafb9b103465
Lukas Möller <lukasmoeller1998@googlemail.com>
2020-11-02 00:01:39 +0100

    Added ViewModel for PDFBatchImportPreview

---
+++ ZebraDesktop/ViewModels/PDFBatchImporterPreviewViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Zebra.PdfHandling;
+
+namespace ZebraDesktop.ViewModels
+{
+    public class PDFBatchImporterPreviewViewModel : ViewModelBase
+    {
+        #region Properties
+        private ImportBatch _batch;
+
+        public ImportBatch Batch
+        {
+            get { return _batch; }
+            set { _batch = value; ListImportAssignments(); UpdateButtonStatus(); NotifyPropertyChanged(); }
+        }
+
+        public DelegateCommand ImportBatchCommand { get; set; }
+        #endregion
+
+        #region Constructors
+
+        public PDFBatchImporterPreviewViewModel()
+        {
+            ImportBatchCommand = new DelegateCommand(executeImportBatchCommand, canExecuteImportBatchCommand);            
+        }
+
+        
+
+        public PDFBatchImporterPreviewViewModel(ImportBatch batch) : this()
+        {
+            Batch = batch;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void ListImportAssignments()
+        {
+            if (Batch == null) return;
+
+            if (!(Batch.importAssignments == null)) Batch.importAssignments.Clear();
+
+            foreach (var candidate in Batch.importCandidates)
+            {
+                if (candidate.IsAssigned)
+                {
+                    Batch.importAssignments.Add(new ImportAssignment(candidate.AssignedPiece, candidate.AssignedPart, new List<int>() { candidate.PageNumber }));
+                }
+            }
+        }
+
+        private bool canExecuteImportBatchCommand(object obj)
+        {
+            return (Batch?.importAssignments.Count > 0);
+        }
+
+        private async void executeImportBatchCommand(object obj)
+        {
+            try
+            {
+                await Batch.ImportAllAssignmentsAsync((Application.Current as App).Manager);
+            }
+            catch (SheetAlreadyExistsException ex)
+            {
+
+                if (MessageBox.Show($"The sheet {ex.ExistingSheet.Piece} - {ex.ExistingSheet.Part} already exists. Would you like to override it?", "Sheet already exists", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No) == MessageBoxResult.Yes)
+                {
+                    await ex.Assignment.ImportAsync((Application.Current as App).Manager, Batch.File, true);
+                }
+            }
+        }
+
+        #endregion
+
+
+    }
+}
--- ZebraDesktop/Views/frmPdfBatchImportPreview.xaml.cs
+++ ZebraDesktop/Views/frmPdfBatchImportPreview.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Zebra.PdfHandling;
+using ZebraDesktop.ViewModels;
 
 namespace ZebraDesktop
 {
@@ -19,35 +20,11 @@ namespace ZebraDesktop
     /// </summary>
     public partial class frmPdfBatchImportPreview : Window
     {
-        public ImportBatch Batch { get; }
-
         public frmPdfBatchImportPreview(ImportBatch _batch)
         {
             InitializeComponent();
+            this.DataContext= new PDFBatchImporterPreviewViewModel(_batch);
 
-            Batch = _batch;
-
-            if(!(Batch.importAssignments == null)) Batch.importAssignments.Clear();
-
-            foreach (var candidate in Batch.importCandidates)
-            {
-                if (candidate.IsAssigned)
-                {
-                    Batch.importAssignments.Add(new ImportAssignment(candidate.AssignedPiece, candidate.AssignedPart, new List<int>(){ candidate.PageNumber }));
-                }
-            }
-
-            lvPreview.ItemsSource = Batch.importAssignments;
-
-        }
-
-        private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var app = (App)Application.Current;
-            var manager = app.Manager;
-
-            await Batch.ImportAllAssignments(manager);
-            
         }
     }
 }
